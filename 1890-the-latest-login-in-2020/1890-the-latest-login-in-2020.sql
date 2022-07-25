e5b69213ff35b0f60e8fd0c1ba945f5c1ec13ecb
Siddharth1060 <109179446+Siddharth1060@users.noreply.github.com>
2022-07-25 01:29:59 -0500

    Time: 1417 ms (8.34%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,14 +1,7 @@
 # Write your MySQL query statement below
 
+select user_id, max(time_stamp) as last_stamp
+from Logins 
+where Year(time_stamp)= 2020
+group by user_id 
 
-
-with rnk as
-    (select user_id, time_stamp,
-            rank() over(partition by user_id order by time_stamp desc) as stamp_rnk
-     from Logins
-     where Year(time_stamp)=2020
-     
-    )
-select user_id, rnk.time_stamp as last_stamp
-from rnk
-where rnk.stamp_rnk =1
