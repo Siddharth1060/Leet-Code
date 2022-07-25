b4a939a8f25bde907322354fe15ba9840ef44d56
Siddharth1060 <109179446+Siddharth1060@users.noreply.github.com>
2022-07-25 02:24:55 -0500

    Time: 476 ms (76.85%), Space: 0B (100.00%) - LeetHub

--- 585-investments-in-2016/585-investments-in-2016.sql
+++ 585-investments-in-2016/585-investments-in-2016.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
-select round(sum(i1.tiv_2016),2) as tiv_2016
-from insurance i1
-where exists ( select pid from insurance  where i1.tiv_2015=tiv_2015 and i1.pid<>pid) 
-      and  not exists ( select pid from insurance  where i1.pid<>pid and i1.lat=lat and i1.lon=lon) 
-
+with CTE as (
+    select tiv_2015, tiv_2016, lat, lon, 
+    count(pid) over (partition by tiv_2015) as count_tid,
+    count(pid) over (partition by lat,lon) as count_loc 
+    from insurance
+)
+select round(sum(tiv_2016),2) as tiv_2016 from CTE where count_tid>1 and count_loc=1
