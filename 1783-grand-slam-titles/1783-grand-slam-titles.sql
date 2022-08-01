8b03e0e5d430337f283a1c329caf057b220c8eae
Siddharth1060 <109179446+Siddharth1060@users.noreply.github.com>
2022-08-01 04:17:03 -0500

    Time: 1160 ms (86.71%), Space: 0B (100.00%) - LeetHub

--- 1783-grand-slam-titles/1783-grand-slam-titles.sql
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-select player_id, player_name, (sum(player_id= wimbledon)+sum(player_id= US_open)+sum(player_id= Fr_open)+sum(player_id= Au_open)) as grand_slams_count 
+select player_id, player_name, 
+       sum( case when player_id = wimbledon then 1 else 0 end +
+            case when player_id = fr_open then 1 else 0 end +
+            case when player_id = us_open then 1 else 0 end +
+            case when player_id = au_open then 1 else 0 end) as grand_slams_count
 from players p join championships c on p.player_id in (c.wimbledon,c.fr_open, c.au_open,c.us_open)
 group by 1
