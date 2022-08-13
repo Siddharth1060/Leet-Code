67142b5213bdf548c5f132a13f2d9c3b0b199c81
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-08-13 10:38:31 -0500

    Time: 560 ms (67.89%), Space: 0B (100.00%) - LeetHub

--- 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
+++ 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-with cte as(
-    select user_id, visit_date,
-            rank() over(partition by user_id order by visit_date)as rnk
-    from uservisits
-)
 
-select c1.user_id, max(datediff(ifnull(c2.visit_date,'2021-1-1'), c1.visit_date)) as biggest_window
-from cte c1 left join cte c2 on c1.user_id = c2.user_id and c1.rnk=c2.rnk-1
-group by c1.user_id
-
+select user_id, max(diff) as biggest_window
+from
+    (select user_id,
+               (datediff
+                (lead(visit_date,1,'2021-1-1') over(partition by user_id order by visit_date), visit_date)) as diff
+     from uservisits
+    ) t
+group by user_id
 
