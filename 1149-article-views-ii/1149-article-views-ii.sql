ae0be1630b53fb730f821d84c69f39a8edc61c32
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-08-29 15:30:41 -0500

    Time: 731 ms (25.23%), Space: 0B (100.00%) - LeetHub

--- 1149-article-views-ii/1149-article-views-ii.sql
+++ 1149-article-views-ii/1149-article-views-ii.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
 select distinct v1.viewer_id as id
-from views v1 join views v2 on v1.viewer_id=v2.viewer_id and v1.article_id<>v2.article_id
-where v1.view_date=v2.view_date
-group by v1.view_date, v1.viewer_id
-having count( v1.viewer_id)>1 
+from views v1 join views v2 on v1.viewer_id=v2.viewer_id and v1.article_id<>v2.article_id and v1.view_date= v2.view_date
 order by 1
