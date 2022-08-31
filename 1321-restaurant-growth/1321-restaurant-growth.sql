39ca6b7d02ad093c8d52ef860398b1b2aac85b1f
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-08-31 15:56:01 -0500

    Time: 732 ms (13.67%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select distinct visited_on,
+       sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as amount,
+       round(sum(amount)OVER(order by visited_on RANGE between interval 6 day preceding and current row)/7,2) as average_amount
+
+from customer
+limit 1000 offset 6
