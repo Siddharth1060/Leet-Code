de3827090ab724cb57cc49bd6afaff0ca848ca05
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-04 20:16:44 -0500

    Time: 937 ms (57.24%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -4,6 +4,5 @@ from seller
 where seller_id not in (select seller_id
                         from orders
                         where year(sale_date)= '2020'
-                        group by seller_id, sale_date
                        )
 order by 1
