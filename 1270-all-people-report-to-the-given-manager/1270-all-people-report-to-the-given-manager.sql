6eb3a6d4e2c9b341d270f169ae590230e277b71b
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-11 23:00:57 -0500

    Time: 268 ms (60.31%), Space: 0B (100.00%) - LeetHub

--- 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -3,7 +3,6 @@
 select employee_id
 from employees 
 where employee_id<> 1 and manager_id in (select e1.employee_id
-                                                from employees e1 join employees e2 on e1.manager_id=e2.employee_id
-                                                where e1.manager_id=1 or e2.manager_id=1
-
-                                             )
+                                         from employees e1 join employees e2 on e1.manager_id=e2.employee_id
+                                         where e1.manager_id=1 or e2.manager_id=1
+                                         )
