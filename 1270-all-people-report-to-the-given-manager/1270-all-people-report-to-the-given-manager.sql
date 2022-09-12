02e580819c6ffd07462c6f461cb26aa6c85cac91
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-11 22:52:58 -0500

    Time: 401 ms (23.89%), Space: 0B (100.00%) - LeetHub

---
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select e1.employee_id
+from employees e1 join employees e2 on e1.manager_id=e2.employee_id
+where e1.employee_id<> 1 and e1.manager_id in (select e1.employee_id
+                                                from employees e1 join employees e2 on e1.manager_id=e2.employee_id
+                                                where e1.manager_id=1 or e2.manager_id=1
+
+                                             )
