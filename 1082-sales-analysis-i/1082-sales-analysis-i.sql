68cbe30e603bf8701dca5c8f6990b345aaffb16b
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-15 01:41:58 -0500

    Time: 1657 ms (26.76%), Space: 0B (100.00%) - LeetHub

---
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select seller_id
+from Sales
+group by seller_id
+having sum(price) = (select sum(price)
+                        from sales
+                        group by seller_id
+                        order by 1 desc    
+                         limit 1    )
+
+
