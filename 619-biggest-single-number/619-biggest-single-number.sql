24e623e5eeb3d12bff9834069cfc4ad87e407f12
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-22 17:20:52 -0500

    Time: 398 ms (63.16%), Space: 0B (100.00%) - LeetHub

---
+++ 619-biggest-single-number/619-biggest-single-number.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+ 
+  select ifnull((select num
+                from mynumbers
+                group by num
+                having count(num)=1
+                order by 1 desc
+                limit 1), null) num
+                            
