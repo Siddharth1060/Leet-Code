be09d867a60116bbedfa410633ce975ea5645263
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-25 21:07:31 -0500

    Time: 2377 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -2,8 +2,7 @@
  select seller_id
 from sales
 group by seller_id
-having sum(price) = (select sum(price)
+having sum(price) >= all(select sum(price)
                      from sales
                      group by seller_id
-                     order by sum(price) desc
-                    limit 1)
+                    )
