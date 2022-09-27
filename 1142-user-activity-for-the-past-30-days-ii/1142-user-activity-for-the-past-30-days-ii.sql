e69c15c858be8cb46c5dba71bdbf02426fcffed0
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-09-27 13:53:19 -0500

    Time: 1009 ms (7.56%), Space: 0B (100.00%) - LeetHub

--- 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
+++ 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
@@ -5,5 +5,5 @@ from (
         select count(distinct session_id) cnt, count(distinct user_id) ucnt
         from activity
         where activity_date between '2019-06-28' and '2019-07-27'
-        group by user_id
+        
         )t
