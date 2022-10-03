3b74fe603815d23fa99abe420b61aa2e594f2864
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-03 15:52:36 -0500

    Time: 1142 ms (9.91%), Space: 0B (100.00%) - LeetHub

--- 550-game-play-analysis-iv/550-game-play-analysis-iv.sql
+++ 550-game-play-analysis-iv/550-game-play-analysis-iv.sql
@@ -1,17 +1,5 @@
 # Write your MySQL query statement below
-with cte as(
-    select *, 
-    ifnull(lead(event_date,1) over(partition by player_id order by event_date asc),0) as nxt,
-    row_number() over(partition by player_id order by event_date asc) as rnk
-    from activity
-    
-  
-),
-cte2 as(
-    select count(distinct player_id) as ttl_cnt
-    from activity
-)
-
-select round(count(player_id)/ ttl_cnt,2) as fraction
-from cte,cte2
-where datediff(nxt,event_date)=1 and rnk=1
+select round(count(t.player_id)/ (select count( distinct player_id) from activity),2) as fraction
+from (select *, lead(event_date,1) over(partition by player_id order by event_date) ld,
+                row_number() over(partition by player_id order by event_date) rnk   from activity)t 
+where datediff(ld, event_date) =1 and rnk=1
