5deccb2f81c0dd8fb1607d8e69c0d9f541e30c36
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-05 17:24:46 -0500

    Time: 762 ms (22.36%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-select id,
-      (case 
-       when p_id is null then 'Root'
-       when id in (select p_id from tree) then 'Inner'
-       when id not in (select p_id from tree where p_id is not null) then 'Leaf'
-       end) as type
+select id , (case 
+            when p_id is null then 'Root'
+            when id in (select p_id from tree)  is not null then 'Inner'
+            else 'Leaf'
+            end) as type
 from tree
+
+
+
