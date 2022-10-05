05b738dbba577b434b86db8aea155ad0c2d9ff1f
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-05 16:50:43 -0500

    Time: 471 ms (89.00%), Space: 0B (100.00%) - LeetHub

--- 585-investments-in-2016/585-investments-in-2016.sql
+++ 585-investments-in-2016/585-investments-in-2016.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-select round(sum(tiv_2016),2) as tiv_2016
-from insurance i1
-where (i1.lat,i1.lon) not in (select lat, lon
-                              from insurance
-                              where pid<> i1.pid
-                             )
-                             
-                        and i1.tiv_2015 in (select tiv_2015
-                                          from insurance
-                                          where pid<> i1.pid
-                                         )
+select round(sum(distinct tiv_2016),2) as tiv_2016
+from insurance  
+where (lat,lon)  in (select lat,lon from insurance 
+                              group by lat,lon
+                              having count(pid) = 1)
+and tiv_2015 in (select tiv_2015 from insurance
+                 group by tiv_2015
+                 having count(pid) > 1)
