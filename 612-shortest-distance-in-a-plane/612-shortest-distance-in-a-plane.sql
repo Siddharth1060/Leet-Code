cd30d522e190a2b33826cef9f93fbb7a6fb1a296
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-06 15:45:41 -0500

    Time: 428 ms (22.78%), Space: 0B (100.00%) - LeetHub

--- 612-shortest-distance-in-a-plane/612-shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/612-shortest-distance-in-a-plane.sql
@@ -1,10 +1,6 @@
 # Write your MySQL query statement below
-with cte as(
-    select  round(sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y)),2) as fun 
-    from Point2d p1, point2d p2
-    where (p1.x,p1.y) <> (p2.x,p2.y)
-)
-select min(fun) as shortest
-from cte
-
-
+select round(sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y-p1.y)*(p2.y-p1.y)),2) as shortest
+from point2d p1 cross join point2d p2 
+where (p1.x,p1.y) <> (p2.x,p2.y)
+order by 1
+limit 1
