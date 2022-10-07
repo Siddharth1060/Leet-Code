5f5e5232bb02192ce0d968ef80dedeac83796484
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-07 17:08:43 -0500

    Time: 2071 ms (13.48%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
-select  t.product_id, 
-        t.year as first_year,
-        t.quantity,
-        t.price
+select product_id, year as first_year, quantity, price
 from
-         (  select product_id,year,quantity,price,
-            rank() over( partition by product_id order by year ) rnk
-            from sales
-         ) as t
-where t.rnk= 1
-         
+(select *, dense_rank() over(partition by product_id order by year) rnk
+from sales)t
+where rnk=1
