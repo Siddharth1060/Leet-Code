849c02e7e493ebd4fb3cf23f9670fc8e65ea0789
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-10 19:55:35 -0500

    Time: 926 ms (10.53%), Space: 0B (100.00%) - LeetHub

--- 1149-article-views-ii/1149-article-views-ii.sql
+++ 1149-article-views-ii/1149-article-views-ii.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 select distinct viewer_id as id
-from views 
-group by viewer_id, view_date
-having count( distinct article_id)>1
-order by 1
+from views
+group by viewer_id,view_date 
+having count(distinct article_id)>1
