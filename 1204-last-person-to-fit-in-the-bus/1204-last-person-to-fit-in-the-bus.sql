af8cf5f5395c8a20fe5c0079f3ccd5b70e114515
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-11 16:56:47 -0500

    Time: 1059 ms (59.04%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-select person_name from
-    (select person_name, weight,sum(weight) over(order by turn) as tot_weight
-     from queue)t
-where tot_weight<=1000
-order by tot_weight desc
-limit 1     
-     
- 
- 
+select person_name
+from 
+(select *, sum(weight) over(order by turn) rolling_weight
+from queue
+)t
+where rolling_weight<=1000
+order by turn desc
+limit 1
