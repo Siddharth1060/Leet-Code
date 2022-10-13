5373e1b23559d7b4f6f33aeb78a8542009bfcf85
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-13 15:40:53 -0500

    Time: 478 ms (87.67%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
+
 select distinct page_id as recommended_page
-from likes  
-where user_id in (select user2_id from friendship   where user1_id=1
-                         union
-                 (select user1_id from friendship where user2_id=1))
-     and page_id not in (select page_id from likes where user_id=1)
-       
+from likes l join
+                (select user2_id as frnd_id
+                from friendship
+                where user1_id=1 
+
+                union
+
+                select user1_id as frnd_id
+                from friendship
+                where user2_id=1) f on l.user_id = f.frnd_id
+where l.page_id not in (select page_id
+                        from likes
+                        where user_id =1
+                       )
