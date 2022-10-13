13a8c8eb8879e3f3fbbc9be17728d8ab6150488a
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-13 16:09:10 -0500

    Time: 288 ms (54.85%), Space: 0B (100.00%) - LeetHub

--- 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-
-select employee_id
-from employees 
-where employee_id<> 1 and manager_id in (select e1.employee_id
-                                         from employees e1 join employees e2 on e1.manager_id=e2.employee_id
-                                         where e1.manager_id=1 or e2.manager_id=1
-                                         )
+select e1.employee_id
+from employees e1 join employees e2 on e1.manager_id=e2.employee_id join employees e3 on e3.employee_id=e2.manager_id
+where e1.employee_id<>1 and (e1.manager_id=1 or e2.manager_id=1 or e3.manager_id=1)
