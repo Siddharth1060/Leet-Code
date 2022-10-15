5ed12c180e71bd49184c06924f03bf0b3afd0aed
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-15 12:49:27 -0500

    Time: 1406 ms (36.79%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,30 +1,23 @@
 # Write your MySQL query statement below
+(
+select name as results
+from
+(select r.user_id, u.name, count(r.movie_id) cnt
+from movierating r join users u on u.user_id=r.user_id
+group by r.user_id
+)t
+order by cnt desc, name
+limit 1
+)
 
-
-
-    (select name as results
-    from
-        (select u.name, count(m.movie_id) as cnt
-        from
-        movierating m join users u on m.user_id = u.user_id
-        group by u.user_id 
-        )t
-    
-    order by cnt desc, name
-     limit 1
-    )
 union
-    (select title as results
-    from 
-        (select mv.title, avg(m.rating) as ag, m.created_at
-        from
-        movierating m join movies mv on mv.movie_id = m.movie_id
-        
-         group by m.movie_id, month(m.created_at)
-         having month(m.created_at)= 02
-        )t
-     order by ag desc, title 
-     limit 1
-     
-     )
 
+select title as results
+from
+(select r.user_id, m.title, avg(rating) ag
+from movierating r join movies m on m.movie_id = r.movie_id
+where month(r.created_at) = 02
+group by m.movie_id
+order by ag desc, title
+limit 1
+)t
