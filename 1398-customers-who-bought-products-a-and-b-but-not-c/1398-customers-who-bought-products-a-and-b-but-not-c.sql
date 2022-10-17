3d8f13caeeacca46ff67f16eef9e240f4cc3c36b
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-17 15:55:19 -0500

    Time: 625 ms (75.15%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,22 +1,19 @@
 # Write your MySQL query statement below
-
-
- select distinct o.customer_id ,c.customer_name
- from orders o join customers c on c.customer_id= o.customer_id
-        group by o.customer_id, product_name
- having o.customer_id not in (select customer_id
-                           from Orders
-                           where product_name ='C'  
-                          ) 
-       and
-       o.customer_id  in (select customer_id
-                           from orders
-                           where product_name ='A'  
-                          ) 
-       and
-       o.customer_id  in (select customer_id
-                           from orders
-                           where product_name ='B'  
-                          ) 
-                          
-                          
+select c.customer_id,c.customer_name
+from orders o join customers c on o.customer_id=c.customer_id
+group by o.customer_id
+having o.customer_id in ( 
+                        select customer_id 
+                        from orders
+                        where product_name= 'A' 
+                        )
+     and o.customer_id in ( 
+                        select customer_id 
+                        from orders
+                        where product_name= 'B' 
+                        )
+    and o.customer_id not in ( 
+                        select customer_id 
+                        from orders
+                        where product_name= 'C' 
+                        )
