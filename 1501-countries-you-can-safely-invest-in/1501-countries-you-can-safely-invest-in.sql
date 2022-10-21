d0dd2ed4da2ff14f1522874e679faad89d917c42
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-20 19:45:15 -0500

    Time: 1259 ms (76.74%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
-
-select c.name as country
-from country c 
-join person p on c.country_code = left(p.phone_number,3)
-join calls cl on cl.caller_id = p.id or cl.callee_id=p.id
-group by c.country_code
-having avg(duration) > (select avg(duration) from calls)
-
-
-
-
+select name as country
+from 
+(select c.name, id , caller_id, callee_id, duration, avg(duration) ag, (select avg(duration) from calls) as glbl
+from country c join person p on c.country_code= substring(p.phone_number,1,3)
+                join calls cl on cl.caller_id=p.id or cl.callee_id= p.id
+group by c.name)t
+where ag> glbl
