11c1fafd6c72de8cd43a215a912bc01bd8ebd220
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-24 15:57:57 -0500

    Time: 1956 ms (34.97%), Space: 0B (100.00%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
@@ -3,5 +3,5 @@ Select i.invoice_id, c.customer_name, i.price, count(contact_name) as contacts_c
        sum(case when contact_email in (select email from customers) then 1 else 0 end) as trusted_contacts_cnt
 from invoices i left join customers c on i.user_id = c.customer_id
                 left join contacts ct on ct.user_id= c.customer_id
-group by ct.user_id, i.invoice_id
+group by i.invoice_id
 order by 1
