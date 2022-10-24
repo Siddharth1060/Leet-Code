7f642c11ca701b44ac86b5a8f61b5de8738da941
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-23 20:37:29 -0500

    Time: 486 ms (41.24%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-select visited_on, amount, average_amount
-from 
-            (select visited_on, sum(amt) over(order by visited_on Rows between 6 preceding and current row) as amount,
-                    round(avg(amt) over(order by visited_on Rows between 6 preceding and current row),2) as average_amount,
-                     row_number() over(order by visited_on) as rno
-            from (select * , sum(amount) amt
-                  from customer
-                  group by visited_on)t
-            ) p
-where p.rno > 6
+select visited_on, 
+        sum(amt) over(order by visited_on Rows between 6 preceding and current row) as amount,
+        round(avg(amt) over(order by visited_on Rows between 6 preceding and current row),2) as average_amount
+from (select * , sum(amount) amt
+      from customer
+      group by visited_on)t
+limit 1000 offset 6 
