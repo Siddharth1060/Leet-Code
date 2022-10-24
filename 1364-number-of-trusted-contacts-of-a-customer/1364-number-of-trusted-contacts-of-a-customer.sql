3bb547a706f36fc514b8ca6e29675519a3026999
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-24 15:55:39 -0500

    Time: 2086 ms (28.22%), Space: 0B (100.00%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-select i.invoice_id,
-       c.customer_name,
-       i.price,
-       count( ct.user_id) as contacts_cnt,
-       sum(case when ct.contact_email in (select distinct email from customers) then 1 else 0 end) as trusted_contacts_cnt
-
+Select i.invoice_id, c.customer_name, i.price, count(contact_name) as contacts_cnt, 
+       sum(case when contact_email in (select email from customers) then 1 else 0 end) as trusted_contacts_cnt
 from invoices i left join customers c on i.user_id = c.customer_id
-                left join contacts ct on ct.user_id= c.customer_id 
-group by 1
-order by invoice_id
+                left join contacts ct on ct.user_id= c.customer_id
+group by ct.user_id, i.invoice_id
+order by 1
