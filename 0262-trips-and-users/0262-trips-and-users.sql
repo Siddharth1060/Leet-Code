530c89d91c4825d37ecb66df03322d6222ff9f1f
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-10-31 18:34:27 -0500

    Time: 1534 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -6,7 +6,7 @@
 
 
 select request_at as Day, 
-       round(sum(case when t.status<>'completed' then 1 else 0 end)/ count(*),2) as 'Cancellation Rate'
+       round(sum(case when t.status<>'completed' then 1 else 0 end)/ count(id),2) as 'Cancellation Rate'
 from Trips t left join users u on t.client_id=u.users_id 
              left join users ud on t.driver_id=ud.users_id
 where u.banned = 'No' and ud.banned='No' and t.request_at between "2013-10-01" and "2013-10-03"
