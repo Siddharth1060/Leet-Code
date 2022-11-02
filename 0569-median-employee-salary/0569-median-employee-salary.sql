2c1a3917d44b4225cf2668dbc77b88eaf3e79d13
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-11-02 15:45:50 -0500

    Time: 511 ms (33.03%), Space: 0B (100.00%) - LeetHub

--- 0569-median-employee-salary/0569-median-employee-salary.sql
+++ 0569-median-employee-salary/0569-median-employee-salary.sql
@@ -6,10 +6,10 @@ from
      count(*) over(partition by company) ttl
      from employee
     )temp
-where rno >= ttl/2 and rno <= ttl/2+1
-#case
-#       # when ttl % 2=0 then rno= round(ttl/2,0) or rno= round(ttl/2,0) + 1
-#       # else rno= round(ttl/2,0)
+where #rno >= ttl/2 and rno <= ttl/2+1
+        case
+      when ttl % 2=0 then rno= round(ttl/2,0) or rno= round(ttl/2,0) + 1
+      else rno= round(ttl/2,0)
 #      when mod(ttl,2) = 0 then (rno = ttl/2 or rno = ttl/2 + 1)
 #      else (rno = floor(ttl/2) + 1 )
-#       end
+       end
