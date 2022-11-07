3745f404821235c802e29b741ea946ebdf334aba
Siddharth Chauhan <109179446+Siddharth1060@users.noreply.github.com>
2022-11-06 23:55:35 -0600

    Time: 604 ms (58.99%), Space: 0B (100.00%) - LeetHub

--- 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select round(sum(case when datediff(customer_pref_delivery_date, order_date) = 0 then 1 else 0 end)*100/ count(delivery_id),2) as immediate_percentage
+select round(sum(case when datediff(customer_pref_delivery_date, order_date)=0 then 1 else 0 end)*100/ count(*),2) as immediate_percentage
 from delivery
